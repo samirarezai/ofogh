276858546e0270486f2a8f9420e1aa432b35869f
Your Namesamirarezaigit config --global user.name Your <you@example.comsamirarezai1996@gmail.comgit config --global user.email you@example.comsamirarezai1996@gmail.com>
2019-07-22 17:59:49 +0430

    header

---
+++ assets/js/javascript.js
@@ -0,0 +1,72 @@
+$(document).ready(function(){
+
+    /*=======================effect for scroll spy=====================*/
+    // Add scrollspy to <body>
+    $('body').scrollspy({target: ".navbar", offset: 50});
+
+    // Add smooth scrolling on all links inside the navbar
+    $(".nav-item .nav-link").on('click', function(event) {
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+            $('.active').removeClass('active');
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 700, function(){
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+                $(this).parent().addClass('active');
+            });
+
+
+
+        }  // End if
+    });
+
+
+
+
+
+//============================responsive-menu-icon==========================//
+    $('.bars').click(function(){
+        if($(this).attr('data-open') == 'open'){
+            $(this).find('.hamburger').addClass('opened');
+            $(this).attr('data-open','close');}
+        else {
+            $(this).find('.hamburger').removeClass('opened');
+            $(this).attr('data-open','open');
+        }
+    });
+//=======================scroll navbar===================//
+    $(window).scroll(function(){
+        var y = $(this).scrollTop();
+        if( $(this).scrollTop() > 99){
+            $('.navigation').addClass('style-navigation');
+            /* $('.navigation .navbar-brand img').css('max-width','50px');*/
+
+        } else if($(this).scrollTop() < 99 ){
+            $('.navigation').removeClass('style-navigation');
+            /*   $('.navigation .navbar-brand img').css('max-width','90px');*/
+
+        }
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+});
